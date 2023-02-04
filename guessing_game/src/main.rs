49c76d04ae2d71e33ad6408e4630a98945842a22
Aamir <djzaamir@gmail.com>
2023-02-04 17:08:45 +0500

    Added a loop to continously ask to guess the numbers

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,22 +3,25 @@ use std::cmp::Ordering;
 use rand::{self, Rng};
 
 fn main() {
-    println!("Please Enter A Number between 1-10:");
-
-    let mut guess : String = String::new();
-    let random_num = rand::thread_rng().gen_range(1..=10);
-
-    io::stdin().read_line(&mut guess)
-        .expect("Unable to read a guess");
     
+    loop {
+        println!("Please Enter A Number between 1-10:");
+        
+        let mut guess : String = String::new();
+        let random_num = rand::thread_rng().gen_range(1..=10);
+
+        io::stdin().read_line(&mut guess)
+            .expect("Unable to read a guess");
+        
 
 
-    //Parse to int
-    let guess : u32 = guess.trim().parse().expect("Please enter a number");
+        //Parse to int
+        let guess : u32 = guess.trim().parse().expect("Please enter a number");
 
-    match guess.cmp(&random_num){
-        Ordering::Less => println!("Your guess is low"),
-        Ordering::Greater => println!("Your guess is high"),
-        Ordering::Equal => println!(">>> Congratulations you have guessed the number <<<")
+        match guess.cmp(&random_num){
+            Ordering::Less => println!("Your guess is low"),
+            Ordering::Greater => println!("Your guess is high"),
+            Ordering::Equal => println!(">>> Congratulations you have guessed the number <<<")
+        }
     }
 }
